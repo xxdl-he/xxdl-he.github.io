11845ec03f935977c041b3111ecffcd6619a25da
xxdl-he <62934330+xxdl-he@users.noreply.github.com>
2020-04-26 06:42:16 +0800

    Add files via upload

---
+++ click.js
@@ -0,0 +1,34 @@
+/* 鼠标特效 */ 
+var a_idx = 0; 
+var b_idx = 0; 
+/*   文字和颜色数组 */
+var a = new Array("🐍", "蛇", "中考", "加油", "arithmetic square root", "贰伍", "壹陆" ,"叁壹", "贰玖", "贰捌", "零陆", "叁伍","你老是点我干🐎？","叁捌","噗嗤噗嗤"); 
+var b = new Array("#FF0000","#FF7F00"," #FFFF00","#00FF00","#00FFFF","#0000FF","#8B00FF","#FF0000","#FF7F00"," #FFFF00","#00FF00","#00FFFF","#0000FF","#8B00FF");
+jQuery(document).ready(function($) { 
+    $("body").click(function(e) { 
+       
+        var $i = $("<span/>").text(a[a_idx]); 
+          a_idx=parseInt(15*Math.random()); //文字随机数
+          b_idx=parseInt(14*Math.random()); //颜色随机数
+        var x = e.pageX, 
+        y = e.pageY; 
+        $i.css({ 
+            "z-index": 999, 
+            "font-size":"1.3em",            //字体大小
+            "top": y - 20, 
+            "left": x, 
+            "position": "absolute", 
+            "font-weight": "bold", 
+            "color": b[b_idx] 
+        }); 
+        $("body").append($i); 
+        $i.animate({ 
+            "top": y - 180, 
+            "opacity": 0 
+        }, 
+        1500, 
+        function() { 
+            $i.remove(); 
+        }); 
+    }); 
+}); 
---
+++ title.js
@@ -0,0 +1,13 @@
+var OriginTitile = document.title;    // 保存之前页面标题  
+var titleTime;  
+document.addEventListener('visibilitychange', function(){  
+    if (document.hidden){  
+        document.title ='你不要我啦？ TT';  
+        clearTimeout(titleTime);  
+    }else{  
+        document.title = '略略略 你回来啦 ';  
+        titleTime = setTimeout(function() {  
+            document.title = OriginTitile;  
+        }, 1000); // 2秒后恢复原标题  
+    }  
+});  
